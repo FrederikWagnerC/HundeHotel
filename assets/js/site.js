7715de63839a6609b869aacd3b35e28b19c8e037
FrederikWagnerC <frederikwagnercs@gmail.com>
2024-05-03 12:59:08 +0200

    Html,css og JS
    Html,css og JS for hele siden

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,2 +1,14 @@
 // write cool JS hwere!!
 
+menuButtonOpen = document.getElementById('menuOpen')
+asideNav = document.getElementById('asideNav')
+asideClose = document.getElementById('asideClose')
+
+
+function menuFunction() {
+    asideNav.classList.toggle('hidden')
+    asideNav.classList.toggle('asideDiv')
+}
+
+menuButtonOpen.addEventListener('click', menuFunction);
+asideClose.addEventListener('click', menuFunction);
